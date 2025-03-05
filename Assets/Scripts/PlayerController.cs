238a50148d8af71eb928f0adc74232f3923d2816
Harrison T <159206613+harry-vt@users.noreply.github.com>
2025-03-06 09:38:59 +1100

    golden ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0; 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb; 
+    private int count; 
     private float movementX;
     private float movementY;
 
@@ -15,6 +19,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
        rb = GetComponent <Rigidbody>();  
+       count = 0;
+
+       SetCountText();
+       winTextObject.SetActive(false);
     }
 
 
@@ -25,8 +33,14 @@ public class PlayerController : MonoBehaviour
           movementX = movementVector.x; 
           movementY = movementVector.y; 
      }
-
-
+       void SetCountText()
+     {
+         countText.text = "Count: " + count.ToString();
+         if(count >= 12) 
+         {
+              winTextObject.SetActive(true);
+         }
+     }
        private void FixedUpdate() 
    {
           Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
@@ -39,6 +53,9 @@ public class PlayerController : MonoBehaviour
          if(other.gameObject.CompareTag("Pickup"))
          {
           other.gameObject.SetActive(false);
+          count = count + 1;
+
+          SetCountText();
          }
          
       }
